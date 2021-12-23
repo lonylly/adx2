cbad9e3d2a6201a2eaef56edd3e0bd9cd85758ee
lonylly <96376524+lonylly@users.noreply.github.com>
2021-12-23 15:56:08 +0200

    Add files via upload

---
+++ ADX/razan/resources/Gif/_gif.py
@@ -0,0 +1,37 @@
+# ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━#
+B = (
+    "- [ADX 𝖴𝖲𝖤𝖱𝖡𝖮T - 𝖠𝖭𝖨𝖬𝖤 𝖦𝖨𝖥](t.me/TEAM_ADX) \n\n"
+    "**⌔∮ قائـمه متحـركات الانمي :**\n\n"
+    "- `.متحركات ولد`"
+)
+
+gifrz_1 = "https://t.me/ADX_GIF/4"
+gifrz_2 = "https://t.me/ADX_GIF/5"
+gifrz_3 = "https://t.me/ADX_GIF/6"
+gifrz_4 = "https://t.me/ADX_GIF/17"
+gifrz_5 = "https://t.me/ADX_GIF/8"
+gifrz_6 = "https://t.me/ADX_GIF/9"
+gifrz_7 = "https://t.me/ADX_GIF/10"
+gifrz_8 = "https://t.me/ADX_GIF/11"
+gifrz_9 = "https://t.me/ADX_GIF/12"
+gifrz_10 = "https://t.me/ADX_GIF/13"
+gifrz_11 = "https://t.me/ADX_GIF/15"
+gifrz_12 = "https://t.me/ADX_GIF/16"
+
+ROZG = (
+    "- [ADX 𝖴𝖲𝖤𝖱𝖡𝖮T - 𝖠𝖭𝖨𝖬𝖤 𝖦𝖨𝖥](t.me/TEAM_ADX) \n\n"
+    "**⌔∮ قائـمه المتحـركات :**\n\n"
+    "- `.و1`\n"
+    "- `.و2`\n"
+    "- `.و3`\n"
+    "- `.و4`\n"
+    "- `.و5`\n"
+    "- `.و6`\n"
+    "- `.و7`\n"
+    "- `.و8`\n"
+    "- `.و9`\n"
+    "- `.و10`\n"
+    "- `.و11`\n"
+    "- `.و12`\n"
+    "- [ADX 𝖴𝖲𝖤𝖱𝖡𝖮T](t.me/TEAM_ADX) \n\n"
+)
